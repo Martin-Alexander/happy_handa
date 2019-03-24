ab57e12d020b53e89460b597367a1e4baea5d95e
Cli <c.someponpakdi@gmail.com>
2019-03-23 22:34:05 -0400

    filled in actions in controller and rerouted routes for goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,17 @@
 class GoalsController < ApplicationController
+  
+  def index
+  end
+
   def new
+  end
+  
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'goals/new'
-  root 'static_pages#home'
-  get 'about', to: 'static_pages#home'
-  get 'contact', to: 'static_pages#contact'
   devise_for :user
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/goals', to: 'goals#index' # list of all goals
+  get '/goals/new', to: 'goals#new' # rendering the form for a new goal
+  post '/goals', to: 'goals#create' # add a new goal to DB
+  get '/goals/:id/edit', to: 'goals#edit' # rendering the form to edit a goal
+  patch '/goals/:id', to: 'goals#update' # edit a goal in the DB
 end
