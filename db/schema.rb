c0dc5b766379fad2bc3d20d6b544a277b0833f9f
Cli <c.someponpakdi@gmail.com>
2019-03-23 22:23:03 -0400

    generated model for goals and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_034048) do
+ActiveRecord::Schema.define(version: 2019_03_24_021951) do
+
+  create_table "goals", force: :cascade do |t|
+    t.string "name"
+    t.boolean "done"
+    t.date "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
